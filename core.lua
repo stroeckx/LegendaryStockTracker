7729e58e7ace23e3e430bfc11a8071d97464bb07
Stroeckx <stijnstroeckx@hotmail.com>
2022-11-17 08:16:59 +0100

    Updated to new container API calls
     credits to Luiz tavarez

--- core.lua
+++ core.lua
@@ -28,7 +28,7 @@ local LstockLDB = LibStub("LibDataBroker-1.1"):NewDataObject("LegendaryStockTrac
   end
 })
 
-local LSTVersion = "v2.21.1"
+local LSTVersion = "v2.21.2"
 --local db = nil
 LST.db = nil
 local LstockIcon = LibStub("LibDBIcon-1.0")
